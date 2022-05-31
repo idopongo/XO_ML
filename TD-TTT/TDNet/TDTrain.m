540608dbd39170a48f79c6b83149f04a2fe4fc58
idopongo <idopongo@gmail.com>
2022-05-31 12:01:54 +0300

    given code

---
+++ TD-TTT/TDNet/TDTrain.m
@@ -0,0 +1,14 @@
+function TDTrain(S, r)
+% Train the TD system
+% S - The states' history for a given trial
+% r - the reward given for each state in the given trial
+
+% Load the network's data if needed
+global Net;
+LoadNet();
+
+% TODO: Implement TD(0). It is highly recommended to read the documentation
+% of TDEvaluate first. 
+
+end
+
---
+++ TD-TTT/TicTacToe/Policies/Softmax.m
@@ -0,0 +1,10 @@
+function num = Softmax(Grades)
+%EPSGREEDY Softmax policy
+% Grades    - The critic grades for each possible action
+% num       - The chosen action's index
+
+% TODO: Implement the softmax policy
+
+num = 1;
+
+end
